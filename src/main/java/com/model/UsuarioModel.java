ea57ff5e9c2546ce740112607f8deb00466fff65
Marco Botelho <marcobotelhoabreu@gmail.com>
2024-01-23 23:06:35 -0300

    Inclusão persistencia hibernate jpa

--- src/main/java/com/model/UsuarioModel.java
+++ src/main/java/com/model/UsuarioModel.java
@@ -1,6 +1,18 @@
 package com.model;
 
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "usuario")
 public class UsuarioModel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
@@ -37,6 +49,28 @@ public class UsuarioModel {
 
 	public void setEmail(String email) {
 		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "UsuarioModel [id=" + id + ", nome=" + nome + ", email=" + email + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		UsuarioModel other = (UsuarioModel) obj;
+		return Objects.equals(id, other.id);
 	}
 
 }
